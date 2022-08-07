b6c8300e754213b699adaaa23a242dafcefc6a36
Oxana Kozhaeva <kozhaeva_1@mail.ru>
2022-08-08 01:20:21 +0300

    Add Hometask1

---
+++ Hometask1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max=a, min=b;
+if(b>max)
+{
+    max=b;
+    min=a;
+}
+Console.Write("max=");
+Console.WriteLine(max);
+Console.Write("min=");
+Console.WriteLine(min);
